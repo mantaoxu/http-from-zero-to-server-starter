4f9dfea51282de90459f12b84060a7ce5b658241
Eduardo <eduardosh1105@gmail.com>
2021-04-12 15:02:30 -0600

    Added codes

--- test/server.js
+++ test/server.js
@@ -1,13 +1,72 @@
-const http = require('http');
+const http = require("http");
 
-http.createServer(function(request, response){
-  if (req.url === '/OK') {
-    console.log('Inbound "OK" request being processed...');
-    response.writeHead(200);
-    response.end();
-  } else {
-    
-  }
-}).listen(3000, function(){
-  console.log('listening on port 3000');
-})
+http
+	.createServer(function (request, response) {
+		if (request.url === "/OK") {
+			console.log('Inbound "OK" request being processed...');
+			response.writeHead(200);
+			response.end();
+		}
+		if (request.url === "/Bad-Request") {
+			console.log("Bad request");
+			response.writeHead(400);
+			response.end();
+		}
+		if (request.url === "/Created") {
+			console.log("Created");
+			response.writeHead(201);
+			response.end();
+		}
+		if (request.url === "/Forbidden") {
+			console.log("Forbidden");
+			response.writeHead(403);
+			response.end();
+		}
+		if (request.url === "/Found") {
+			console.log("Found");
+			response.writeHead(302);
+			response.end();
+		}
+		if (request.url === "/Gateway-Timeout") {
+			console.log("Gateway Timeout");
+			response.writeHead(504);
+			response.end();
+		}
+		if (request.url === "/Internal-Server-Error") {
+			console.log("Internal Server Error");
+			response.writeHead(500);
+			response.end();
+		}
+		if (request.url === "/Moved-Permanently") {
+			console.log("Moved Permanently");
+			response.writeHead(301);
+			response.end();
+		}
+		if (request.url === "/Unauthorized") {
+			console.log("Unauthorized");
+			response.writeHead(401);
+			response.end();
+		} else {
+			console.log("404");
+			response.writeHead(404);
+			response.end();
+		}
+		if (request.url === "/Bonus/Redirect") {
+			console.log("");
+			response.writeHead();
+			response.end();
+		}
+		if (request.url === "") {
+			console.log("");
+			response.writeHead();
+			response.end();
+		}
+		if (request.url === "") {
+			console.log("");
+			response.writeHead();
+			response.end();
+		}
+	})
+	.listen(3000, function () {
+		console.log("listening on port 3000");
+	});
